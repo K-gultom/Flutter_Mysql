003bf21641bc3b2f24fafacffbf0ecd2eb7250cf
K-gultom <yehezkielgg28@gmail.com>
2023-06-17 10:48:54 +0700

    commit model

--- flutter_mysql/lib/models/note.dart
+++ flutter_mysql/lib/models/note.dart
@@ -9,20 +9,4 @@ class Note {
     required this.content,
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'title': title,
-      'content': content,
-    };
-  }
-
-  factory Note.fromMap(Map<String, dynamic> map) {
-    return Note(
-      id: map['id'],
-      title: map['title'],
-      content: map['content'],
-    );
-  }
-
 }
